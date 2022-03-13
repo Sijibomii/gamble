018494061f313a506b67a27bd270840cded72d50
unknown <sijibomiolajubu@gmail.com>
2022-03-13 12:26:43 +0100

    templates and staticfiles

--- app/app.py
+++ app/app.py
@@ -1,13 +1,11 @@
 from flask import Flask
-
+from app.blueprints.page import page
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object('config.settings')
     app.config.from_pyfile('settings.py', silent=True)
 
-    @app.route('/')
-    def index():
-        return 'Hello World!'
+    app.register_blueprint(page)
 
     return app
