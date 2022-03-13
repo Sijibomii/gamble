ca4e13a2ff97dfdd2708065cd560548babe4c075
unknown <sijibomiolajubu@gmail.com>
2022-03-13 15:18:26 +0100

    create app ishh

--- app/app.py
+++ app/app.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from app.blueprints.page import page
 
-myapp = create_app()
-
 def create_app():
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object('config.settings')
-    app.config.from_pyfile('settings.py', silent=True)
+  app = Flask(__name__, instance_relative_config=True)
+  app.config.from_object('config.settings')
+  app.config.from_pyfile('settings.py', silent=True)
 
-    app.register_blueprint(page)
+  app.register_blueprint(page)
 
-    return app
+  return app
+
+myapp = create_app()
